f64714e3027db2e29587746df2f314eb77010dce
TSezari Kvelashvili <cez3x.business@gmail.com>
2023-08-05 12:22:31 +0400

    fixed bugs

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const Blog = require('./models/blog.js')
 
 const app = express()
 
+// connect to database
+mongoose.connect(process.env.dbURL)
+    .then(() => {
+        app.listen(process.env.PORT, (req, res) => {
+            console.log(`listening for requests on port ${process.env.PORT}`)
+        })
+    })
+    .catch(err => console.log(err))
+
+
 // middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // routes
 app.get('/', (req, res) => {
@@ -16,13 +27,33 @@ app.get('/', (req, res) => {
 })
 
 app.get('/blogs', (req, res) => {
-    res.render('blogs', { title: 'Blogs' })
+    Blog.find()
+        .then(result => {
+            res.render('blogs', { title: 'Blogs', blogs: result })
+        })
+        .catch(err => console.log(err))
 })
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'New Blog' })
 })
 
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`listening for requests on port ${process.env.PORT}`)
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findById(id)
+        .then(result => {
+            res.render('blog', { title: result.title, blog: result})
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then(result => {
+            console.log('blog saved!')
+            res.redirect('/blogs')
+        })
+        .catch(err => console.log(err))
 })
